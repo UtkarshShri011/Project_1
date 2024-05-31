4f6dd017041c9d94537741c99355112cdcb39392
Utkarsh Shrivastava <ushrivastava@deqode.com>
2024-05-31 18:28:05 +0530

    people page added

--- node_rest-api/index.js
+++ node_rest-api/index.js
@@ -8,4 +8,5 @@ const PORT = 3000;
 //Routes
 app.get('/', (req, res) => {res.send('Hello World')});
 // call back functions
+app.get('/people',(req,res) =>{res.send( "People Were Here")});
 app.listen(PORT, () => console.log('Server is started at the Port 3000'));
