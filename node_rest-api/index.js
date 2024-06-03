4c2892b98fec0efaa7f84508914972f255e93cb9
Utkarsh Shrivastava <ushrivastava@deqode.com>
2024-06-03 11:22:05 +0530

    connected with db

--- node_rest-api/index.js
+++ node_rest-api/index.js
@@ -1,12 +1,46 @@
 //This is for the Setting the  server
 
 const express = require("express");
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    user: 'postgres',
+    password:'utkarsh',
+    host: 'localhost',
+    database:"school",
+    port:'5432'
+});
+
+
 
 const app =  express();
 const PORT = 3000;
+app.use(express.json()); //middleware
 
 //Routes
 app.get('/', (req, res) => {res.send('Hello World')});
 // call back functions
 app.get('/people',(req,res) =>{res.send( "People Were Here")});
+
+
+app.get('/api/student',(req,res) => {   
+     pool.query('SELECT * FROM student',(error, results)=>{
+    if(error) throw error;
+    res.status(200).json(results.rows);
+   } )
+});
+
+
+app.get('/api/teacher',(req,res) => {   
+    pool.query('SELECT * FROM faculty',(error, results)=>{
+   if(error) throw error;
+   res.status(200).json(results.rows);
+  } )
+});
+
+
+
+
+
+
 app.listen(PORT, () => console.log('Server is started at the Port 3000'));
