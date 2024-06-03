90248d91a0ca93c2d1c7ae7231bd5e53fa6df430
Utkarsh Shrivastava <ushrivastava@deqode.com>
2024-06-03 11:52:39 +0530

    added_post_method

--- node_rest-api/index.js
+++ node_rest-api/index.js
@@ -12,14 +12,13 @@ const pool = new Pool({
 });
 
 
-
 const app =  express();
 const PORT = 3000;
 app.use(express.json()); //middleware
 
 //Routes
 app.get('/', (req, res) => {res.send('Hello World')});
-// call back functions
+// what is call back functions ?
 app.get('/people',(req,res) =>{res.send( "People Were Here")});
 
 
@@ -38,9 +37,12 @@ app.get('/api/teacher',(req,res) => {
   } )
 });
 
-
-
-
+app.post('/api/addstudent',(req,res) => {
+      pool.query('INSERT INTO student VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',['4','harshit','S','1234567890','UP','India'],(error, results)=>{
+        if(error) throw error;
+        res.status(200).json(results.rows);
+       })
+});
 
 
 app.listen(PORT, () => console.log('Server is started at the Port 3000'));
